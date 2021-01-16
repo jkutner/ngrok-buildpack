1dd212013cfcb2b66cdf060a1211131fd667c4f9
Joe Kutner <jpkutner@gmail.com>
2021-01-16 11:35:08 -0600

    Support better config

--- sbin/ngrok.sh
+++ sbin/ngrok.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 if [ -n "$NGROK_TOKEN" ]; then
-  ngrok_cmd="ngrok start --config .ngrok2/ngrok.yml -authtoken $NGROK_TOKEN -log stdout --log-level debug ${NGROK_OPTS:-}"
+  local config
+  if [ ! -f .ngrok2/ngrok.yml ]; then
+    mkdir -p .ngrok2
+    cat <<TOML > .ngrok2/ngrok.yml
+tunnels:
+  web:
+    proto: http
+    addr: ${PORT:-8080}
+TOML
+  fi
+
+  ngrok_cmd="ngrok start --config .ngrok2/ngrok.yml -authtoken $NGROK_TOKEN -log stdout --log-level debug ${NGROK_OPTS:---all}"
   echo "[$(log_date)] INFO  Starting ngrok on port ${mc_port}..."
   eval "$ngrok_cmd | tee ngrok.log &"
   ngrok_pid=$!
